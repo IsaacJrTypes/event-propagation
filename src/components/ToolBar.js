587eaea4e48eb94f92d7a86ce5cd2686c81b942d
Isaac J <duckhunterjr@gmail.com>
2024-02-22 00:44:28 -0800

    add dynamic buttons

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,11 +1,13 @@
 import AlertButton from "./AlertButton";
+import { buttons } from "../data/buttons";
 export default function ToolBar() {
     return (
         <div>
-            <AlertButton msg='quack'>Animal 1</AlertButton>
+            {/* <AlertButton msg='quack'>Animal 1</AlertButton>
             <AlertButton msg='moo'>Animal 2</AlertButton>
             <AlertButton msg='woof'>Animal 3</AlertButton>
-            <AlertButton msg='YEEEEHAAA'>Animal 4</AlertButton>
+            <AlertButton msg='YEEEEHAAA'>Animal 4</AlertButton> */}
+            {buttons.map((btn) => btn.message && btn.children && <AlertButton msg={btn.message}>{btn.children}</AlertButton>)}
         </div>
     );
 }
---
+++ src/data/buttons.js
@@ -0,0 +1,10 @@
+export const buttons = [
+    { message: 'Downloading!', children: 'Download File' },
+    { message: 'Sharing!', children: 'Share Document' },
+    { message: 'REEYAWOOO!!!', children: 'Step on cat\'s tail' },
+    { message: 'PURRRRRRR!!!', children: 'Pet cat' },
+    { message: '', children: '' },
+    { message: 'Nada' },
+    { children: '' },
+
+];
